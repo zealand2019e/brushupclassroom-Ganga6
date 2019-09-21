063c679f9abe879a17e935d3440bfc78acbc222e
Ganga Ammay <ammayganga50@gmail.com>
2019-09-21 10:41:04 +0200

    Student class with their attributes and constructor.

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+      public  string name { get; set; }
+       public int birthMonth { get; set; }
+       public int birthday { get; set; }
+
+       public Student(string name, int birthMonth, int birthday)
+       {
+           this.name = name;
+           this.birthMonth = birthMonth;
+           this.birthday = birthday;
+       }
+    }
+}
